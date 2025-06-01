bc8b54608e09076aea2e4befc9ab3455900c6f35
钱多多 <14382551+Moneyfool@user.noreply.gitee.com>
2025-06-02 00:17:09 +0800

    chore: add test-app project to test monitor dev

--- apps/admin/types/auto/svg-component-global.d.ts
+++ apps/admin/types/auto/svg-component-global.d.ts
@@ -7,20 +7,61 @@
 import 'vue'
 declare module 'vue' {
   export interface GlobalComponents {
-    SvgIcon: import("vue").DefineComponent<{
+    SvgIcon: import('vue').DefineComponent<
+      {
         name: {
-            type: import("vue").PropType<"dashboard" | "fullscreen-exit" | "fullscreen" | "keyboard-down" | "keyboard-enter" | "keyboard-esc" | "keyboard-up" | "search">;
-            default: string;
-            required: true;
-        };
-    }, {}, unknown, {}, {}, import("vue").ComponentOptionsMixin, import("vue").ComponentOptionsMixin, {}, string, import("vue").VNodeProps & import("vue").AllowedComponentProps & import("vue").ComponentCustomProps, Readonly<import("vue").ExtractPropTypes<{
-        name: {
-            type: import("vue").PropType<"dashboard" | "fullscreen-exit" | "fullscreen" | "keyboard-down" | "keyboard-enter" | "keyboard-esc" | "keyboard-up" | "search">;
-            default: string;
-            required: true;
-        };
-    }>>, {
-        name: "dashboard" | "fullscreen-exit" | "fullscreen" | "keyboard-down" | "keyboard-enter" | "keyboard-esc" | "keyboard-up" | "search";
-    }>;
+          type: import('vue').PropType<
+            | 'dashboard'
+            | 'fullscreen-exit'
+            | 'fullscreen'
+            | 'keyboard-down'
+            | 'keyboard-enter'
+            | 'keyboard-esc'
+            | 'keyboard-up'
+            | 'search'
+          >
+          default: string
+          required: true
+        }
+      },
+      {},
+      unknown,
+      {},
+      {},
+      import('vue').ComponentOptionsMixin,
+      import('vue').ComponentOptionsMixin,
+      {},
+      string,
+      import('vue').VNodeProps & import('vue').AllowedComponentProps & import('vue').ComponentCustomProps,
+      Readonly<
+        import('vue').ExtractPropTypes<{
+          name: {
+            type: import('vue').PropType<
+              | 'dashboard'
+              | 'fullscreen-exit'
+              | 'fullscreen'
+              | 'keyboard-down'
+              | 'keyboard-enter'
+              | 'keyboard-esc'
+              | 'keyboard-up'
+              | 'search'
+            >
+            default: string
+            required: true
+          }
+        }>
+      >,
+      {
+        name:
+          | 'dashboard'
+          | 'fullscreen-exit'
+          | 'fullscreen'
+          | 'keyboard-down'
+          | 'keyboard-enter'
+          | 'keyboard-esc'
+          | 'keyboard-up'
+          | 'search'
+      }
+    >
   }
 }
