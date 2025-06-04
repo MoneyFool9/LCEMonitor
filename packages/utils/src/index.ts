b8ae57fbf17d08fd731a4030991d6b3c23f3e129
钱多多 <14382551+Moneyfool@user.noreply.gitee.com>
2025-06-04 11:16:57 +0800

    feat(other): add timeGenerater function in packages/utils

--- packages/utils/src/index.ts
+++ packages/utils/src/index.ts
@@ -1,3 +1,12 @@
-export function add(x: number, y: number): number {
-  return x + y
+import dayjs from 'dayjs'
+/**
+ * generate a timestamp string into an ISO 8601 date string.
+ *
+ * @export
+ * @param {}
+ * @returns {string}
+ */
+export function generateTimeStamp(): string {
+  const date = new Date()
+  return dayjs(date.toISOString()).format('YYYY-MM-DD HH:mm:ss')
 }
